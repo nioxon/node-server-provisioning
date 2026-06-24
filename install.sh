51d65ffe276ed7e21743815443cfb48c13229c9e
Developer <developer@gmail.com>
2026-06-24 18:06:54 +0530

    LAN Config Bug Fix

--- install.sh
+++ install.sh
@@ -33,8 +33,10 @@ else
     git clone https://github.com/nioxon/node-server-provisioning.git /opt/nioxon
   else
     cd /opt/nioxon
+    git reset --hard HEAD
+    git clean -fd -e config/
     git fetch --all
-    git reset --hard origin/main
+    git reset --hard origin/$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "main")
   fi
 fi
 
