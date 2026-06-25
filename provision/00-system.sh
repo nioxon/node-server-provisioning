c68fee6fbc085cbba7f4bfa687a3afe5d91160e1
Developer <developer@gmail.com>
2026-06-25 12:30:59 +0530

    SSL Build

--- provision/00-system.sh
+++ provision/00-system.sh
@@ -4,6 +4,6 @@ set -e
 echo "🔄 Updating system and installing base packages..."
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get upgrade -y
+#apt-get upgrade -y
 apt-get install -y --no-install-recommends software-properties-common unzip zip curl ca-certificates \
   nano vim net-tools lsof iputils-ping dnsutils findmnt
