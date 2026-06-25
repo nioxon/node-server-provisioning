800be9f5a73dc954f8e379a03da9ae33a8fbbdd9
Developer <developer@gmail.com>
2026-06-25 12:58:15 +0530

    Database User Creation Fix

--- provision/13-ott-api.sh
+++ provision/13-ott-api.sh
@@ -130,7 +130,8 @@ $ARTISAN artisan key:generate --force
 # -------------------------
 echo "▶ Creating dedicated MySQL user and privileges..."
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_USER_PASS';"
+mysql -u root -e "ALTER USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_USER_PASS}';"
+#mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_USER_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
